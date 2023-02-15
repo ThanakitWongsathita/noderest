a3aa9735688e0aac642e6542f613c399fb4c7f32
ThanakitWongsathita <thanakitwongsathita2@gmail.com>
2023-02-15 11:11:22 +0700

    update

--- src/crudBookNoDb.js
+++ src/crudBookNoDb.js
@@ -58,5 +58,5 @@ app.delete("/books/:id", (req, res) => {
   res.send(book);
 });
 
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
